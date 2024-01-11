f7744157ca4619bf8c14f21d05d4661652a7d25a
tomma <32663244+tomma-a@users.noreply.github.com>
2024-01-11 11:34:41 +0800

    Update build.sh
    added support ssl for http and tcp stream

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #inside a alpine docker
-apk add build-base
+apk add build-base openssl-devel openssl-libs-static
 wget https://nginx.org/download/nginx-1.25.3.tar.gz
  tar xzf nginx-1.25.3.tar.gz
  cd nginx-1.25.3
-  ./configure --without-http_rewrite_module --without-http_gzip_module --with-cc-opt="-O2" --with-ld-opt="-s -static" && make && make install
+  ./configure --with-http_ssl_module --with-stream_ssl_module --without-http_rewrite_module --without-http_gzip_module --with-cc-opt="-O2" --with-ld-opt="-s -static" && make && make install
